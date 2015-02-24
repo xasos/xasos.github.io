8f60328f4571072667cf61bf835bb70365fbc5fe
Niraj Pant <nirajpant7@gmail.com>
2015-02-23 17:18:27 -0800

    cleanup

--- js/init.js
+++ js/init.js
@@ -1,9 +1,3 @@
-/*
-	Astral 2.5 by HTML5 UP
-	html5up.net | @n33co
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
-
 skel.init({
 	prefix: 'css/style',
 	pollOnce: true,
